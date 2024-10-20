918a6a31c780aded9b198e605cc437c4072c5812
Triatmono <162248336+Triatmono@users.noreply.github.com>
2024-10-21 00:07:22 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var conferenceName = "Go Conference"
+	
+	fmt.Println("Welcome to", conferenceName, "booking application!")
+	fmt.Println("Get your ticket now")
+
+	
+	
+}
